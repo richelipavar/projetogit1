cf9cb5a72041a4114dcf70bf81cac8f9c3ce02d0
Richeli Pavarini <richelipavar@gmail.com>
2022-04-21 21:54:52 -0300

    commit inicial

---
+++ Lista01Ex03.py
@@ -0,0 +1,8 @@
+'''
+3.	Escreva um programa que leia um
+valor em metros e o exiba convertido
+em milímetros.
+'''
+
+m = float(input("Iforme a medida em metros >> "))
+print(f"{m} metros = {m*1000} milímetros")
